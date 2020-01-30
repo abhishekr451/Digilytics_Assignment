912a7618c39ea2d0a25e98395661dbc1aa5e2bf5
abhishekr <abhishekr>
2020-01-30 12:58:55 +0530

    Sorting Completed

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit,Renderer2 } from '@angular/core';
+import { ApiService } from '../../Services/Api.service'
 
 @Component({
   selector: 'app-home',
@@ -7,11 +8,62 @@ import { Component, OnInit,Renderer2 } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private renderer: Renderer2) { }
+  constructor(private renderer: Renderer2,private api: ApiService) { }
   loginClass = "login-page";
   test=6253
+  filter=[false,false,false,true]
+  flightDetails=[]
   ngOnInit() {
     this.renderer.removeClass(document.body, this.loginClass)
+    this.api.GetJson().subscribe(res=>{
+     this.flightDetails= res.Data
+     this.Sort()
+    })
+  }
+
+  SwitchFilter(value){
+    this.filter[value]=!this.filter[value]
+    this.Sort()
+  }
+
+  Sort(){
+    let flightDetail=this.flightDetails
+    let sum=this.flightDetails
+    for(var i=0;i<flightDetail.length;i++){
+      for(var j=0;j<this.filter.length;j++){
+        if(this.filter[j]){
+          if(j==0){
+             sum[i]['order']=(+flightDetail[i]["departure"].replace(':','')) + (+sum[i]['order'])
+          }
+          else if(j==1){
+            sum[i]['order']=(+flightDetail[i]["duration"]) + (+sum[i]['order'])
+          }
+          else if(j==2){
+            sum[i]['order']=(+flightDetail[i]["arrival"].replace(':','')) + (+sum[i]['order'])
+          }
+          else if(j==3){
+            sum[i]['order']=(+flightDetail[i]["Price"]) + (+sum[i]['order'])
+          }
+        }
+      }
+    }
+    this.flightDetails=sum.sort(dynamicSort('order'))
+    function dynamicSort(property) {
+      var sortOrder = 1;
+      if(property[0] === "-") {
+          sortOrder = -1;
+          property = property.substr(1);
+      }
+      return function (a,b) {
+          var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+          return result * sortOrder;
+      }
+  }
+  this.flightDetails.map(function(x){
+    x.order=0
+    return x;
+  })
+  console.log(this.flightDetails)
   }
 
 }
